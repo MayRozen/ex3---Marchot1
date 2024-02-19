2753e094c1ecd40654032e679fd68d92354f64cc
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-02-19 23:00:06 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include "StrList.h"
+#include "StrList.c"
 
 
 int main(){
-
+StrList* StrList;
     StrList_alloc(); //Creating a new list.
     int input;
+    char* str;
+    int index;
     // int graph[GRAPH_SIZE][GRAPH_SIZE];
 
     do{
@@ -13,63 +16,79 @@ int main(){
         switch (input)
         {
         case 1:
-            putInValues(graph);
-            floydWarshall(graph);
+            scanf(" %s", str);
+            StrList_insertLast(StrList, str);
             break;
 
         case 2:
-            ifPathItoJ(graph);
+            scanf(" %d", &index);
+            scanf(" %s", str);
+            trList_insertAt(StrList,str,index);
             break;
 
         case 3:
-            shortestPath(graph);
+            StrList_print(StrList);
             break;
 
         case 4:
+            StrList_size(StrList);
             return 0;
             break;
 
         case 5:
+            scanf(" %d", &index);
+            StrList_printAt(StrList,index);
             return 0;     
             break;
 
 
         case 6:
+            StrList_printLen(StrList);
             return 0;     
             break;
 
 
         case 7:
+            scanf(" %s", str);
+            StrList_count( StrList,str);
             return 0;     
             break;
 
 
         case 8:
+            scanf(" %s", str);
+            StrList_remove(StrList,str);
             return 0;     
             break;
 
 
         case 9:
+            scanf(" %d", index);
+            StrList_removeAt(StrList,index);
             return 0;     
             break;
 
 
         case 10:
+            StrList_reverse(StrList);
             return 0;     
             break;
 
 
         case 11:
+            StrList_clone(StrList);
             return 0;     
             break;
 
 
         case 12:
+            StrList_sort(StrList);
             return 0;     
             break;
 
 
         case 13:
+            StrList_isSorted(StrList);
             return 0;     
             break;
 
@@ -81,9 +100,7 @@ int main(){
             break;
         } 
     }
-    while(1);
+    while(input!=0);
     
 return 0;
 }
-
-
